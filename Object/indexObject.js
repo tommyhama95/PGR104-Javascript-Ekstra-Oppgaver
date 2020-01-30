dfca32d8fcc9a361dca72c5e4fa98c47cfe5c64c
tommyhama95 <42606938+tommyhama95@users.noreply.github.com>
2020-01-30 19:30:42 +0100

    Object started

---
+++ Object/indexObject.js
@@ -0,0 +1,26 @@
+// Lage et object
+var example = {};
+
+// Kan brukes for å forklare en person eller noe annet
+example = {
+    name: "Tommy",
+    age: 24,
+    from: "Rjukan",
+    Country: "Norway",
+    sex: "Male"
+};
+
+// Hus
+example = {
+    height: 9,
+    length: 15,
+    width: 23,
+    color: "Blue",
+    material: "Wood and bricks"
+}
+
+// For å hente en spesifik property må du først
+// skrive navnet på variabelen som er objektet
+// og så skrive navnet på propertien
+console.log(example.height); // Denne har nå hentet høyden på huset
+
