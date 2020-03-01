ea1617939815cc87437ff35925f63c8138b18a84
tommyhama95 <42606938+tommyhama95@users.noreply.github.com>
2020-03-01 13:43:17 +0100

    Started on Loops

---
+++ Loops/indexLoops.js
@@ -0,0 +1,13 @@
+// Vi har to forskjellige løkker/loops som vi kan bruke; for og while
+
+/*****  For loops/løkker  *****/
+
+// Bruker nøkkelorder ¨for¨ for å kunne si til javascript at vi skal lage en loop/løkke
+
+/* Den kjennerelle code strukturen
+    for(start kondisjon; mens dette er sant/true; oppførsel) {
+        Koden som skal kjøres x antall ganger som vi spesifiserer inne i () over.
+    }
+*/
+
+/*****  While loops/løkker  *****/
